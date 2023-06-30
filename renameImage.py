c1c5d67aae0698022bb2cad49f37bdaa142a22f2
cuoicungtui <tuongdangvuongquoc901@gmail.com>
2023-06-30 18:41:27 +0700

    update build

--- renameImage.py
+++
@@ -1,9 +0,0 @@
-import os
-folder = './image_test'
-index =1
-for filename in os.listdir(folder):
-    if filename.lower().endswith(".png"):   
-        old_path = folder+'/'+filename
-        new_path = folder+'/'+str(index)+'.png'
-        os.rename(old_path,new_path)
-        index +=1
